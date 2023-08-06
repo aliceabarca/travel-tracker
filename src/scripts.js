aec4faa28c452a4162aa7020e4fc296ee25c6d49
Alice Abarca <aliceabarca@otmail.com>
2023-08-05 23:34:19 -0600

    Add missing syntax

--- src/scripts.js
+++ src/scripts.js
@@ -5,8 +5,8 @@
 import './css/styles.css';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
-import './images/bikes.jpg'
+import './images/turing-logo.png';
+import './images/bikes.jpg';
 
 import { arrFetch } from '../dist/api';
 
@@ -17,12 +17,12 @@ import { cards, usersDestination } from '../dist/domUpdates';
 
 let currentTraveler = {
   id: 2,
-  name: "Rachael Vaughten",
-  travelerType: "thrill-seeker"
-}
+  name: 'Rachael Vaughten',
+  travelerType: 'thrill-seeker',
+};
 
 let currentTravelerTrips;
-export let travelFeePrecentage = 0.10;
+export let travelFeePrecentage = 0.1;
 
 export const usersData = {
   user: null,
@@ -33,23 +33,21 @@ export const usersData = {
     pending: [],
     upcoming: [],
   },
-  destinations: []
-}
-
+  destinations: [],
+};
 
 window.addEventListener('load', () => {
-  Promise.all(arrFetch)
-  .then(results => {
-    const allUsersTrips = results[1].trips
-    const tripsDestinations = results[2].destinations
+  Promise.all(arrFetch).then(results => {
+    const allUsersTrips = results[1].trips;
+    const tripsDestinations = results[2].destinations;
     // usersData.travelers = results[0]
-    usersData.trips = filteredTrips(currentTraveler.id, allUsersTrips)
-    usersData.destinations = results[2]
-    const __ = calculateTripsCost(usersData.trips.all, tripsDestinations)
-    console.log(usersData)
+    usersData.trips = filteredTrips(currentTraveler.id, allUsersTrips);
+    usersData.destinations = results[2];
+    const __ = calculateTripsCost(usersData.trips.all, tripsDestinations);
+    console.log(usersData);
     // currentTravelerTrips = usersTrip(currentTraveler.id, usersData.trips)
     // cards(currentTravelerTrips)
     // usersDestination(trips, destinations)
-  })
-})
+  });
+});
 console.log('This is the JavaScript entry file - your code begins here.');
--- src/travelData.js
+++ src/travelData.js
@@ -1,32 +1,32 @@
-import { travelFeePrecentage } from "./scripts";
+import { travelFeePrecentage } from './scripts';
 
 export function filteredTrips(userId, allTrips) {
   const userTrips = allTrips.filter(trip => {
-    return trip.userID === userId
+    return trip.userID === userId;
   });
 
   const pendingTrips = userTrips.filter(trip => {
-   return trip.status === 'pending'
-  })
+    return trip.status === 'pending';
+  });
 
   const pastTrips = userTrips.filter(trip => {
-    const tripDate = new Date(trip.date).getTime()
-    const todaysDate = new Date().getTime()
-    return tripDate < todaysDate && trip.status === 'approved'
-  })
+    const tripDate = new Date(trip.date).getTime();
+    const todaysDate = new Date().getTime();
+    return tripDate < todaysDate && trip.status === 'approved';
+  });
 
   const upcomingTrips = userTrips.filter(trip => {
-    const tripDate = new Date(trip.date).getTime()
-    const todaysDate = new Date().getTime()
-    return tripDate > todaysDate && trip.status === 'approved'
-  })
+    const tripDate = new Date(trip.date).getTime();
+    const todaysDate = new Date().getTime();
+    return tripDate > todaysDate && trip.status === 'approved';
+  });
 
   return {
     all: userTrips,
     pending: pendingTrips,
     past: pastTrips,
     upcoming: upcomingTrips,
-  }
+  };
 }
 
 export function calculateTripsCost(usersTrip, destinations) {
@@ -34,27 +34,27 @@ export function calculateTripsCost(usersTrip, destinations) {
 
   const thisYearsTrips = usersTrip.filter(trip => {
     const tripYear = new Date(trip.date).getFullYear();
-    return tripYear === currentYear
-  })
+    return tripYear === currentYear;
+  });
 
   let costOfAllTrips = 0;
 
   thisYearsTrips.forEach(trip => {
-    const destID = trip.destinationID
-    const duration = trip.duration
-    
+    const destID = trip.destinationID;
+    const duration = trip.duration;
+
     destinations.forEach(location => {
-      const costPerDay = location.estimatedLodgingCostPerDay
-      const flightCost = location.estimatedFlightCostPerPerson
+      const costPerDay = location.estimatedLodgingCostPerDay;
+      const flightCost = location.estimatedFlightCostPerPerson;
       if (destID === location.id) {
-        const travelCost = costPerDay * duration
-        const cost = travelCost + flightCost
-        costOfAllTrips += cost
+        const travelCost = costPerDay * duration;
+        const cost = travelCost + flightCost;
+        costOfAllTrips += cost;
       }
-    })
-  })
+    });
+  });
 
-  const agentsFee = costOfAllTrips * travelFeePrecentage
+  const agentsFee = costOfAllTrips * travelFeePrecentage;
 
-  return agentsFee
+  return agentsFee;
 }
