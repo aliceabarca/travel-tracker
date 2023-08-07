9c4a970497f172ada2621fc65fbc8d8880e38af6
Alice Abarca <aliceabarca@otmail.com>
2023-08-07 13:49:59 -0600

    Edit mispelled parameter name for post

--- dist/api.js
+++ dist/api.js
@@ -10,21 +10,50 @@ export const arrFetch = [
   fetchTrips('destinations'),
 ];
 
-export const setApiData = (id, userID, destinationID, travelers, date, duration, status, suggestedActivies) => {
+export const setApiData = (id, userID, destinationID, travelers, date, duration, status, suggestedActivities) => {
+
+const postApi = {
+id: id,
+userID: userID,
+destinationID: destinationID,
+travelers: travelers,
+date: date,
+duration: duration,
+status: status,
+suggestedActivities: suggestedActivities
+}
+
   return fetch(`http://localhost:3001/api/v1/trips`, {
     method: 'POST',
-    body: JSON.stringify({
-    id: id,
-    userID: userID,
-    destinationID: destinationID,
-    travelers: travelers,
-    date: date,
-    duration: duration,
-    status: status,
-    suggestedActivies: suggestedActivies
-  }),
-}),
-  {
-    "Content-Type": "application/json"
-    }
-  }
+    body: JSON.stringify(
+      postApi
+  ),
+  headers: {
+     "Content-Type": "application/json"
+     }
+})
+}
+
+// export const postVacationRequest = (vacation, userID,tripID) => {
+//   let data = {
+//       id: 204, 
+//       userID: 1, 
+//       destinationID: 1, 
+//       travelers: 1, 
+//       date: '2023/09/07', 
+//       duration: 1, 
+//       status: 'pending', 
+//       suggestedActivities: [],
+//   }
+//   return fetch(`http://localhost:3001/api/v1/trips`, {
+//       method: 'POST',
+//       body: JSON.stringify(data),
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//     })
+//     .catch((error) => {
+//       alert(error);
+//     });
+  
+// }
