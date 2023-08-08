cfcc257cfd92f6ae1751791ca6204c29beef2041
Alice Abarca <aliceabarca@otmail.com>
2023-08-08 11:44:42 -0600

    Added test for sample data to check pending and approved trip status

--- test/unit-testing.js
+++ test/unit-testing.js
@@ -1,13 +1,39 @@
-import { expect } from 'chai';
-import { filteredTrips } from '../src/travelData';
+import chai  from 'chai';
+const expect = chai.expect
+import sampleData  from '../test/sampleData'
+
+import {calculateTripsCost, filteredTrips} from '../src/travelData';
+
 
 describe('trips', () => {
-  let trips;
-
-  beforeEach('init data', () => {
-    trips = filteredTrips(2, 'pending')
+  it('should be a function', () => {
+    expect(typeof filteredTrips).to.equal('function')
   })
-  it('should return pending trips', () => {
-    expect(trips.status).to.equal('filter')
+
+  it('should return user pending', () => {
+    const pendingTrip = filteredTrips(44, sampleData.usersTrips)
+
+    expect(pendingTrip.pending).to.deep.equal([ {id:1,
+      userID:44,
+      destinationID:49,
+      travelers:1,
+      date:"2022/09/16",
+      duration:8,
+      status:"pending",
+      suggestedActivities:[]}])
+  });
+
+  it('should return user approved', () => {
+    const approvedTrip = filteredTrips(48, sampleData.usersTrips)
+
+    expect(approvedTrip.past).to.deep.equal([{id:1,
+      userID:48,
+      destinationID:49,
+      travelers:1,
+      date:"2022/09/16",
+      duration:8,
+      status:"approved",
+      suggestedActivities:[]
+    }])
   })
 })
