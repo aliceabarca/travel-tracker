2a5b48f5801b460e4e1bad4e0224e9ea037ae2f7
Alice Abarca <aliceabarca@otmail.com>
2023-08-08 20:00:56 -0600

    Add more test to test my functions in traveldata

--- test/unit-testing.js
+++ test/unit-testing.js
@@ -4,6 +4,12 @@ import sampleData  from '../test/sampleData'
 
 import {calculateTripsCost, filteredTrips} from '../src/travelData';
 
+import travelerData from './travel-data';
+import destinationData from './destination-data';
+import tripData from './trip-data';
+
+// let dataModel = sampleData
+// console.log('hello',dataModel[1])
 
 describe('trips', () => {
   it('should be a function', () => {
@@ -53,16 +59,62 @@ describe('trips', () => {
   })
 
   it('should return incorrect if users trips does not exist', () => {
-    const incorrectMessage = filteredTrips(1)
+    const incorrectMessage = filteredTrips(52)
 
-    expect(incorrectMessage).to.equal('Incorrect')
+    expect(incorrectMessage).to.equal('No user found')
   });
 })
 
 describe('calculate trips', () => {
+
+let sampleTrips1
+let sampleTrips2
+
+let sampleCurrentDestination
+const travelNum = destinationData[0]
+const flightCost = travelNum.estimatedFlightCostPerPerson
+const lodgingCost = travelNum.estimatedLodgingCostPerDay
+
+
+
+  sampleCurrentDestination = {
+    id: 49,
+    destination: 'Miami, Florida',
+    estimatedLodgingCostPerDay: 158,
+    estimatedFlightCostPerPerson: 275,
+    image: 'https://images.unsplash.com/photo-1514214246283-d427a95c5d2f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1573&q=80',
+    alt: 'sand with palm trees and tall buildings in the background'
+  }
+
+  sampleTrips1 = {
+    travelers: 2,
+    duration: 8,
+  }
+
+  sampleTrips2 = {
+    travelers: 0,
+    duration: 7,
+  }
+
   it('should be a function', () => {
     expect(typeof calculateTripsCost).to.equal('function')
   })
-
   
+  it('should calculate flight cost per person', () => {
+    expect(flightCost).to.equal(830)
+  })
+  
+  it('should calculate lodging cost per day', () => {
+    expect(lodgingCost).to.equal(70)
+  })
+
+  it('should return a number', () => {
+    const cost = calculateTripsCost(tripData, destinationData, 2021)
+    expect(cost).to.equal('0.00')
+  })
+
+  it('should return error if data incorrect', () => {
+    const errorMessage = calculateTripsCost(tripData)
+    expect(errorMessage).to.equal('Missing Information')
+  })
 })
